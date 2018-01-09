40acbf94ed233dcb177f7304db5126fd41b36f05
Yuhuaihy <31414395+Yuhuaihy@users.noreply.github.com>
2018-01-08 21:47:21 -0500

    O(n) solution
    use map
    java map.get complexity O(1), overall O(n)

--- easy/1. Two Sum/solution.java
+++ easy/1. Two Sum/solution.java
@@ -33,3 +33,23 @@ class Solution {
 }    
     return re;}
 }
+
+/////////// use map
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int[] result = new int[2];
+        for(int i = 0; i<nums.length; i++){
+            if(map.containsKey(target-nums[i])){
+                result[0]=map.get(target-nums[i]);
+                result[1]=i;
+                return result;
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return new int[]{0,0};
+        
+    }
+}

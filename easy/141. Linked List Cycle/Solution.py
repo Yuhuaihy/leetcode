a920a3ae751f32b0122c3c71ac008590cbba6e6b
Yuhuaihy <31414395+Yuhuaihy@users.noreply.github.com>
2018-03-14 22:43:40 +0800

    Create Solution.py

---
+++ easy/141. Linked List Cycle/Solution.py
@@ -0,0 +1,29 @@
+#Given a linked list, determine if it has a cycle in it.
+
+#Follow up:
+#Can you solve it without using extra space?
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        linkA = head;
+        linkB = head;
+        while linkA and linkB and linkB.next:
+            linkA = linkA.next
+            linkB = linkB.next.next
+            if(linkA == linkB):
+                return True
+            
+        return False
+        
+        

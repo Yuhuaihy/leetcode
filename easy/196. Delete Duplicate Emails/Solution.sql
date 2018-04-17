4ea5dc8af0f04586f470b5fef8f69fff247959f2
Yuhuaihy <31414395+Yuhuaihy@users.noreply.github.com>
2018-04-17 17:17:03 +0800

    Update Solution.sql

--- easy/196. Delete Duplicate Emails/Solution.sql
+++ easy/196. Delete Duplicate Emails/Solution.sql
@@ -1,3 +1,13 @@
 # Write your MySQL query statement below
 delete a from Person a, Person  b
 where a.Id > b.Id and a.Email = b.Email
+
+
+#####group by
+delete from Person
+where Id not in(
+select * from (
+select min(Id) from Person
+    Group by Email
+)temp
+)
